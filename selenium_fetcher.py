dc372a3d761ee3ca1a2ac00aca40d54f0aedda3c
zdhubi <zdenek.hubalek@gmail.com>
2025-07-09 11:32:47 +0200

    Update selenium_fetcher.py

--- selenium_fetcher.py
+++ selenium_fetcher.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from time import sleep
 
 def fetch_products():
     url = "https://www.aunika.com/"
@@ -7,15 +9,21 @@ def fetch_products():
     options.add_argument("--headless")
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
+    options.add_argument("--window-size=1920,1080")
 
     driver = webdriver.Chrome(options=options)
     driver.get(url)
+    sleep(3)
 
-    items = driver.find_elements_by_css_selector(".product-item")
+    items = driver.find_elements(By.CSS_SELECTOR, ".product-item")
     products = []
     for el in items:
-        title = el.find_element_by_css_selector(".product-title").text
-        price = el.find_element_by_css_selector(".price").text
+        title_el = el.find_element(By.CSS_SELECTOR, ".product-title")
+        price_el = el.find_element(By.CSS_SELECTOR, ".price")
+
+        title = title_el.text.strip()
+        price = price_el.text.strip()
         products.append({"title": title, "price": price})
+
     driver.quit()
     return products
